c5f62d143cf06ed50a1d54b205287bee3c69fbaa
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-08-03 21:12:03 +0200

    Service for Trips

---
+++ src/main/java/com/backend/service/DbService.java
@@ -0,0 +1,32 @@
+package com.backend.service;
+
+import com.backend.domain.Trip;
+import com.backend.exception.TripNotFoundException;
+import com.backend.repository.TripRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DbService {
+
+    private final TripRepository repository;
+
+    public List<Trip> getAllTrips() {
+        return repository.findAll();
+    }
+
+    public Trip getTrip(final Long tripId) throws TripNotFoundException {
+        return repository.findById(tripId).orElseThrow(TripNotFoundException::new);
+    }
+
+    public Trip saveTrip(final Trip trip) {
+        return repository.save(trip);
+    }
+
+    public void deleteTrip(final Long tripId) {
+        repository.deleteById(tripId);
+    }
+}
