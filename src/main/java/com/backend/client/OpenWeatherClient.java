c344501f02e8a87ddb0461564551af5919115bf9
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-08-14 13:04:11 +0200

    Added client for OpenWeather API

---
+++ src/main/java/com/backend/client/OpenWeatherClient.java
@@ -0,0 +1,36 @@
+package com.backend.client;
+
+import com.backend.config.OpenWeatherConfig;
+import com.backend.dto.WeatherForecastDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Component
+@RequiredArgsConstructor
+public class OpenWeatherClient {
+
+    private final OpenWeatherConfig openWeatherConfig;
+    private final RestTemplate restTemplate;
+
+    public WeatherForecastDto getWeatherForecast(String location) {
+        URI url = UriComponentsBuilder
+                .fromHttpUrl(openWeatherConfig.getOpenWeatherForecastApiEndpoint())
+                .queryParam("q", location)
+                .queryParam("appid", openWeatherConfig.getOpenWeatherAppKey())
+                .queryParam("units", "metric")
+                .build()
+                .encode()
+                .toUri();
+        try {
+            return restTemplate.getForObject(url, WeatherForecastDto.class);
+        } catch (RestClientException e) {
+            System.out.println("Error " + e);
+            return null;
+        }
+    }
+}
