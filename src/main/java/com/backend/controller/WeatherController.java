4620d503f9e2ffd5abe183d9aad4a9e3c22cdc93
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-08-21 17:41:29 +0200

    Added GET method for average temp for 5 days

--- src/main/java/com/backend/controller/WeatherController.java
+++ src/main/java/com/backend/controller/WeatherController.java
@@ -1,7 +1,7 @@
 package com.backend.controller;
 
-import com.backend.client.OpenWeatherClient;
 import com.backend.dto.WeatherForecastDto;
+import com.backend.service.OpenWeatherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +12,16 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class WeatherController {
 
-    private final OpenWeatherClient openWeatherClient;
+    private final OpenWeatherService openWeatherService;
 
     @GetMapping("forecast/{location}")
     public ResponseEntity<WeatherForecastDto> getWeatherForecast(@PathVariable String location) {
-        return ResponseEntity.ok(openWeatherClient.getWeatherForecast(location));
+        return ResponseEntity.ok(openWeatherService.fetchWeatherForecast(location));
+    }
+
+    @GetMapping("forecast/{location}/temperature")
+    public ResponseEntity<Double> getAverageTemp(@PathVariable String location) {
+        WeatherForecastDto forecastDto = openWeatherService.fetchWeatherForecast(location);
+        return ResponseEntity.ok(openWeatherService.calculateAverageTemperatureFor5Days(forecastDto));
     }
 }
