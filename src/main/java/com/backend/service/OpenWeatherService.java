138bb6d91515fe9ec5c67c6c250f1c88583d2e65
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-08-21 17:43:11 +0200

    Created service for OpenWeather

---
+++ src/main/java/com/backend/service/OpenWeatherService.java
@@ -0,0 +1,33 @@
+package com.backend.service;
+
+import com.backend.client.OpenWeatherClient;
+import com.backend.dto.WeatherForecastDto;
+import com.backend.openweather.List;
+import com.backend.openweather.Main;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.OptionalDouble;
+
+@Service
+@RequiredArgsConstructor
+public class OpenWeatherService {
+
+    private final OpenWeatherClient openWeatherClient;
+
+    public WeatherForecastDto fetchWeatherForecast(String location) {
+        return openWeatherClient.getWeatherForecast(location);
+    }
+
+    public double calculateAverageTemperatureFor5Days(WeatherForecastDto forecastDto) {
+        OptionalDouble averageTemp = forecastDto.getList().stream()
+                .map(List::getMain)
+                .mapToDouble(Main::getTemp)
+                .average();
+        if (averageTemp.isPresent()) {
+            return averageTemp.getAsDouble();
+        } else {
+            return 0;
+        }
+    }
+}
